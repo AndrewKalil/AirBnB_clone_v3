c1114c7d14a12c369939e1de4a09af0e4b2fc16a
AndrewKalil <1541@holbertonschool.com>
2020-09-09 14:33:55 -0500

    adding error; not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -14,10 +14,12 @@ from models import storage
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
+
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 host = getenv('HBNB_API_HOST', '0.0.0.0')
 port = getenv('HBNB_API_PORT', 5000)
@@ -25,6 +27,13 @@ port = getenv('HBNB_API_PORT', 5000)
 def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """
+    404 error page in JSON
+    """
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == '__main__':
 	app.run(host, port, threaded=True)
